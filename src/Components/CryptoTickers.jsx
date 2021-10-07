f22cc9237fbbb5801d571884b1af099a0049b13d
Bryan Jazo <bryanjazo88@gmail.com>
2021-10-06 17:46:21 -0700

    ajd

--- src/Components/CryptoTickers.jsx
+++ src/Components/CryptoTickers.jsx
@@ -3,24 +3,26 @@ import "./CryptoTicker.css";
 export default function CryptoTickers(crypto) {
   return (
     <div className="header">
-      {crypto.crypto.map((arr) =>
-        arr.map((crypto) => (
-          <div className="crypto">
-            <ul className="navMenu">
-              <div className="div-menu">
-                <li className="navLink">
-                  <img
-                    className="Crypto-Img"
-                    src={crypto.logo_url}
-                    alt={crypto.name}
-                  />
-                  {crypto.symbol} {Math.floor(crypto.price)}
-                </li>
-              </div>
-            </ul>
-          </div>
-        ))
-      )}
+      <div className="hmove">
+        {crypto.crypto.map((arr) =>
+          arr.map((crypto) => (
+            <div className="crypto">
+              <ul className="navMenu">
+                <div className="div-menu">
+                  <li className="navLink">
+                    <img
+                      className="Crypto-Img"
+                      src={crypto.logo_url}
+                      alt={crypto.name}
+                    />
+                    {crypto.symbol} {Math.floor(crypto.price)}
+                  </li>
+                </div>
+              </ul>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
