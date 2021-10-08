0e225d7abe92353f000a9b8090c6fbc0f13f2cf6
Bryan Jazo <bryanjazo88@gmail.com>
2021-10-08 15:11:10 -0700

    kfkd

--- src/Components/Card/HeadlineCard.jsx
+++ src/Components/Card/HeadlineCard.jsx
@@ -1,27 +1,22 @@
-import './HeadlineCard.css';
+import "./HeadlineCard.css";
 
-export default function HeadlineCard(){
-    return(
-        <div>
-            <div className="hl-card">
-                <div className="header">
-                    <h1> Bitcoin Skyrockets Over $50,000.00 </h1>
-                </div>
-                <div className="sub-header">
-                    <p className="catagory">
-                        Category
-                    </p>
-                    <p className="date">
-                        Date
-                    </p>
-                    <p className="author">
-                        Author
-                    </p>
-                </div>
-                <div className="hl-card-img">
-                    <img src="" alt="" />
-                </div>
-            </div>
+export default function HeadlineCard() {
+  return (
+    <div>
+      <div className="hl-card">
+        <div className="header">
+          <h1> Bitcoin Skyrockets Over $50,000.00 </h1>
         </div>
-    )
+        <div className="sub-header">
+          <p className="catagory">Category</p>
+          <p className="date">Date</p>
+          <p className="author">Author</p>
+        </div>
+        <div className="hl-card-img">
+          <img src="" alt="" />
+        </div>
+        <div className="dropDown"></div>
+      </div>
+    </div>
+  );
 }
