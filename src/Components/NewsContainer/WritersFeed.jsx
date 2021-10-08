0847a7c49d376174c11b4c963a8f5bedc347c945
Bryan Jazo <bryanjazo88@gmail.com>
2021-10-08 16:22:31 -0700

    finished bottom section

---
+++ src/Components/NewsContainer/WritersFeed.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RecentsCard from "../Card/RecentsCard";
+export default function WritersFeed() {
+  return (
+    <div className="recentSection">
+      <h3 className="recentsTitle">WRITERS FEED</h3>
+      <RecentsCard />
+    </div>
+  );
+}
