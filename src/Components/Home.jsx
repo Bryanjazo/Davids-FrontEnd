7fd1044bc1d4dbfd58d5148377096e41a0eefb05
Alicia Weenum <weenum.alicia@gmail.com>
2021-10-08 14:58:17 -0400

    css changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,12 @@ import { useDispatch } from "react-redux";
 import { getcryptos, getnewsArticles } from "../Redux/CryptoReduxer";
 import { useSelector } from "react-redux";
 import CryptoTickers from "./CryptoTickers";
-import NewsArticles from "./NewsArticle/NewsArticles";
+import NewsArticles from "./NewsContainer/NewsArticles";
+import Features from "./NewsContainer/Features"
+import Recents from "./NewsContainer/Recents"
+import './Home.css'
+
+
 export default function Home() {
   const { cryptos, newsArticles } = useSelector((state) => state.crypto);
 
@@ -15,9 +20,14 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
+    <div className="home">
       <CryptoTickers crypto={cryptos} />
+      <div className='split'>
       <NewsArticles news={newsArticles} />
+      <Features/>
+      <Recents/>
+      </div>
+      
     </div>
   );
 }
---
+++ src/Components/NewsContainer/Features.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './features.css'
+
+export default function Features() {
+
+
+    return (
+        <div className="featuresSection">
+            <h3 className="featureTitle">Features</h3>
+                <div className="feature">
+
+                </div>
+        </div>
+    )
+}
