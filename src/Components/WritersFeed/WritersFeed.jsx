22336b115fe515afd58b622cf637601cd0eedd96
Bryan Jazo <bryanjazo88@gmail.com>
2021-10-09 19:34:02 -0700

    Finished mid section

---
+++ src/Components/WritersFeed/WritersFeed.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import LifeStyleText from "../LifestyleContainer/LifeStyleText";
+import "./WritersFeed.css";
+export default function WritersFeed() {
+  return (
+    <div className="recentSection">
+      <h3 className="recentsTitleWriters">WRITERS FEED</h3>
+      <div className="writers-container">
+        <div className="writers-Header">
+          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+        </div>
+        <div className="WritersPContainer">
+          <div className="writers-author">
+            <b>
+              <p className="writers-p">Holly Moore</p>
+            </b>
+          </div>
+          <div className="writers-paragraph">
+            <p className="writers-p">April 19, 2021</p>
+          </div>
+        </div>
+      </div>
+      <div className="writers-container">
+        <div className="writers-Header">
+          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+        </div>
+        <div className="WritersPContainer">
+          <div className="writers-author">
+            <p className="writers-p">Holly Moore</p>
+          </div>
+          <div className="writers-paragraph">
+            <p className="writers-p">April 19, 2021</p>
+          </div>
+        </div>
+      </div>
+      <div className="writers-container">
+        <div className="writers-Header">
+          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+        </div>
+        <div className="WritersPContainer">
+          <div className="writers-author">
+            <p className="writers-p">Holly Moore</p>
+          </div>
+          <div className="writers-paragraph">
+            <p className="writers-p">April 19, 2021</p>
+          </div>
+        </div>
+      </div>
+      <div className="writers-container">
+        <div className="writers-Header">
+          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+        </div>
+        <div className="WritersPContainer">
+          <div className="writers-author">
+            <p className="writers-p">Holly Moore</p>
+          </div>
+          <div className="writers-paragraph">
+            <p className="writers-p">April 19, 2021</p>
+          </div>
+        </div>
+      </div>
+      <div className="writers-container">
+        <div className="writers-Header">
+          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+        </div>
+        <div className="WritersPContainer">
+          <div className="writers-author">
+            <p className="writers-p">Holly Moore</p>
+          </div>
+          <div className="writers-paragraph">
+            <p className="writers-p">April 19, 2021</p>
+          </div>
+        </div>
+      </div>
+      <div className="writers-container">
+        <div className="writers-Header">
+          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+        </div>
+        <div className="WritersPContainer">
+          <div className="writers-author">
+            <p className="writers-p">Holly Moore</p>
+          </div>
+          <div className="writers-paragraph">
+            <p className="writers-p">April 19, 2021</p>
+          </div>
+        </div>
+      </div>
+      <div className="writers-container">
+        <div className="writers-Header">
+          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+        </div>
+        <div className="WritersPContainer">
+          <div className="writers-author">
+            <p className="writers-p">Holly Moore</p>
+          </div>
+          <div className="writers-paragraph">
+            <p className="writers-p">April 19, 2021</p>
+          </div>
+        </div>
+      </div>
+      <div className="writers-container">
+        <div className="writers-Header">
+          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+        </div>
+        <div className="WritersPContainer">
+          <div className="writers-author">
+            <p className="writers-p">Holly Moore</p>
+          </div>
+          <div className="writers-paragraph">
+            <p className="writers-p">April 19, 2021</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
