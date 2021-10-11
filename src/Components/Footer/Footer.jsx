c2e2670fa47c71baf4ecf41adcb68cf7fd147ff0
Bryan Jazo <bryanjazo88@gmail.com>
2021-10-10 19:32:28 -0700

    finished cards

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,80 +1,70 @@
 import React from "react";
 
-import './footer.css'
+import "./footer.css";
 
 import {
-Box,
-Container,
-Row,
-Column,
-FooterLink,
-Heading,
+  Box,
+  Container,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
 } from "./FooterStyles";
 
-export default function Footer(){
-return (
-	<Box>
-
-	<h1 style={{color:"white"}}>Logo goes here</h1>
-	<h1  style={{ color: "green",
-				textAlign: "center",
-				marginTop: "-50px" }}>
-	</h1>
-	<Container>
-		<Row>
-		<Column>
-			<Heading>About Us</Heading>
-			<FooterLink href="#">Aim</FooterLink>
-			<FooterLink href="#">Vision</FooterLink>
-			<FooterLink href="#">Testimonials</FooterLink>
-		</Column>
-		<Column>
-			<Heading>Services</Heading>
-			<FooterLink href="#">Writing</FooterLink>
-			<FooterLink href="#">Internships</FooterLink>
-			<FooterLink href="#">Coding</FooterLink>
-			<FooterLink href="#">Teaching</FooterLink>
-		</Column>
-		<Column>
-			<Heading>Contact Us</Heading>
-			<FooterLink href="#">Uttar Pradesh</FooterLink>
-			<FooterLink href="#">Ahemdabad</FooterLink>
-			<FooterLink href="#">Indore</FooterLink>
-			<FooterLink href="#">Mumbai</FooterLink>
-		</Column>
-		<Column>
-			<Heading>Social Media</Heading>
-			<FooterLink href="#">
-			<i className="fab fa-facebook-f">
-				<span style={{ marginLeft: "10px" }}>
-				Facebook
-				</span>
-			</i>
-			</FooterLink>
-			<FooterLink href="#">
-			<i className="fab fa-instagram">
-				<span style={{ marginLeft: "10px" }}>
-				Instagram
-				</span>
-			</i>
-			</FooterLink>
-			<FooterLink href="#">
-			<i className="fab fa-twitter">
-				<span style={{ marginLeft: "10px" }}>
-				Twitter
-				</span>
-			</i>
-			</FooterLink>
-			<FooterLink href="#">
-			<i className="fab fa-youtube">
-				<span style={{ marginLeft: "10px" }}>
-				Youtube
-				</span>
-			</i>
-			</FooterLink>
-		</Column>
-		</Row>
-	</Container>
-	</Box>
-);
-};
+export default function Footer() {
+  return (
+    <Box>
+      <h1 style={{ color: "white" }}>Logo goes here</h1>
+      <h1
+        style={{ color: "green", textAlign: "center", marginTop: "-50px" }}
+      ></h1>
+      <Container>
+        <Row>
+          <Column>
+            <Heading>About Us</Heading>
+            <FooterLink href="#">Aim</FooterLink>
+            <FooterLink href="#">Vision</FooterLink>
+            <FooterLink href="#">Testimonials</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Services</Heading>
+            <FooterLink href="#">Writing</FooterLink>
+            <FooterLink href="#">Internships</FooterLink>
+            <FooterLink href="#">Coding</FooterLink>
+            <FooterLink href="#">Teaching</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Contact Us</Heading>
+            <FooterLink href="#">Uttar Pradesh</FooterLink>
+            <FooterLink href="#">Ahemdabad</FooterLink>
+            <FooterLink href="#">Indore</FooterLink>
+            <FooterLink href="#">Mumbai</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Social Media</Heading>
+            <FooterLink href="#">
+              <i className="fab fa-facebook-f footer">
+                <span style={{ marginLeft: "10px" }}>Facebook</span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-instagram footer">
+                <span style={{ marginLeft: "10px" }}>Instagram</span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-twitter footer">
+                <span style={{ marginLeft: "10px" }}>Twitter</span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-youtube footer">
+                <span style={{ marginLeft: "10px" }}>Youtube</span>
+              </i>
+            </FooterLink>
+          </Column>
+        </Row>
+      </Container>
+    </Box>
+  );
+}
--- src/Components/TeamContainer/TeamCard.jsx
+++ src/Components/TeamContainer/TeamCard.jsx
@@ -1,40 +1,42 @@
-import "./TeamCard.css"
-import user from "../images/user.png"
-import React from 'react';
-import {
-    FacebookIcon,
-    LinkedinIcon,
-    TwitterIcon,
-} from 'react-share';
+import "./TeamCard.css";
+import user from "../images/user.png";
+import React from "react";
+import { FacebookIcon, LinkedinIcon, TwitterIcon } from "react-share";
 
 import {
-    FacebookShareButton,
-    LinkedinShareButton,
-    TwitterShareButton
-} from 'react-share';
-
-export default function TeamCard(){
-        return(
-            <div id="c-body">
-                <div id="card-container">
-                    <div class="card">
-                        <img src={user} id="c-img" alt="..."/>
-                        <div class="name">Name</div>
-                        <share>
-                            <FacebookShareButton id="share" url="">
-                                <FacebookIcon id="share" size={35} round={true}/>
-                            </FacebookShareButton>
-
-                            <LinkedinShareButton id="share" url="">
-                                <LinkedinIcon id="share" size={35} round={true} />
-                            </LinkedinShareButton>
-
-                            <TwitterShareButton id="share" url="">
-                                <TwitterIcon id="share" size={35} round={true} />
-                            </TwitterShareButton>
-                        </share>
-                    </div>
-                </div>
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+} from "react-share";
+import { SocialIcon } from "react-social-icons";
+export default function TeamCard() {
+  return (
+    <div id="c-body">
+      <div id="card-container">
+        <div class="card">
+          <img src={user} id="c-img" alt="..." />
+          <div class="name">Name</div>
+          <div class="subHeader">
+            <p>Position</p>
+          </div>
+          <share>
+            <div>
+              <div className="socialItems">
+                <i class="fab fa-facebook fa-2x social"></i>
+              </div>
+              <div className="socialItems">
+                <i class="fab fa-linkedin fa-2x social"></i>
+              </div>
+              <div className="socialItems">
+                <i class="fab fa-instagram fa-2x social"></i>
+              </div>
+              <div className="socialItems">
+                <i class="fab fa-youtube fa-2x social"></i>
+              </div>
             </div>
-    )
+          </share>
+        </div>
+      </div>
+    </div>
+  );
 }
