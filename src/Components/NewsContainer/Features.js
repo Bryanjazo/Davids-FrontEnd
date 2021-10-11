eb8bc1a27efa29f2867e4052dac4a6d7dc9f924a
Bryan Jazo <bryanjazo88@gmail.com>
2021-10-11 12:23:52 -0700

    fixed title of features

--- src/Components/NewsContainer/Features.js
+++ src/Components/NewsContainer/Features.js
@@ -11,19 +11,13 @@ export default function Features() {
         <div className="header">
           <h1>
             {" "}
-            Bitcoin Rockets to Over $50k as ‘Uptober’ Rally Prompts All-Time
-            High Predictions{" "}
+            Charles Payne from Fox Business with guests David Gokhshtein and
+            Jason Williams{" "}
           </h1>
         </div>
         <div className="sub-header">
-          <p className="category">
-            <i class="far fa-clone"></i> BlockChain
-          </p>
           <p className="date">
-            <i class="far fa-calendar"></i> October 6,2021
-          </p>
-          <p className="author">
-            <i class="far fa-user-circle"></i> Author
+            <i class="far fa-calendar"></i> May 7, 2021
           </p>
         </div>
         <div className="img">
