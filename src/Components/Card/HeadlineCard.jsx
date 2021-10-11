e13775c2a4da6e67477bc6fcabb76fd0e62ff9ae
Bryan Jazo <bryanjazo88@gmail.com>
2021-10-10 21:37:07 -0700

    finished card

--- src/Components/Card/HeadlineCard.jsx
+++ src/Components/Card/HeadlineCard.jsx
@@ -74,6 +74,29 @@ export default function HeadlineCard() {
           <img src={test} alt="" id="img" />
         </div>
       </div>
+      <div className="hl-card-headline">
+        <div className="header">
+          <h1>
+            {" "}
+            Bitcoin Rockets to Over $50k as ‘Uptober’ Rally Prompts All-Time
+            High Predictions{" "}
+          </h1>
+        </div>
+        <div className="sub-header">
+          <p className="category">
+            <i class="far fa-clone"></i> BlockChain
+          </p>
+          <p className="date">
+            <i class="far fa-calendar"></i> October 6,2021
+          </p>
+          <p className="author">
+            <i class="far fa-user-circle"></i> Author
+          </p>
+        </div>
+        <div className="img">
+          <img src={test} alt="" id="img" />
+        </div>
+      </div>
     </div>
   );
 }
