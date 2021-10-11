5efc83d613ebb8e0a67045ef4d885d52a624f8eb
Alicia Weenum <weenum.alicia@gmail.com>
2021-10-11 06:30:11 -0400

    lorem

--- src/Components/MidSection/WritersFeed/WritersFeed.jsx
+++ src/Components/MidSection/WritersFeed/WritersFeed.jsx
@@ -22,92 +22,92 @@ export default function WritersFeed() {
       </div>
       <div className="writers-container">
         <div className="writers-Header">
-          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+          <h1>Nibh Nisl Condimentum Id Venenatis</h1>
         </div>
         <div className="WritersPContainer">
           <div className="writers-author">
-            <p className="writers-p">Holly Moore</p>
+            <p className="writers-p">Lectus Magna</p>
           </div>
           <div className="writers-paragraph">
-            <p className="writers-p">April 19, 2021</p>
+            <p className="writers-p">April 15, 2021</p>
           </div>
         </div>
       </div>
       <div className="writers-container">
         <div className="writers-Header">
-          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+          <h1>Vel Risus Commodo Viverra Mecenas</h1>
         </div>
         <div className="WritersPContainer">
           <div className="writers-author">
-            <p className="writers-p">Holly Moore</p>
+            <p className="writers-p">Lacus Vel</p>
           </div>
           <div className="writers-paragraph">
-            <p className="writers-p">April 19, 2021</p>
+            <p className="writers-p">April 10, 2021</p>
           </div>
         </div>
       </div>
       <div className="writers-container">
         <div className="writers-Header">
-          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+          <h1>Aliquam Sem et Tortor Consequat Id</h1>
         </div>
         <div className="WritersPContainer">
           <div className="writers-author">
             <p className="writers-p">Holly Moore</p>
           </div>
           <div className="writers-paragraph">
-            <p className="writers-p">April 19, 2021</p>
+            <p className="writers-p">April 7, 2021</p>
           </div>
         </div>
       </div>
       <div className="writers-container">
         <div className="writers-Header">
-          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+          <h1>Porta Nibh Venenatis Cras Sed Felis</h1>
         </div>
         <div className="WritersPContainer">
           <div className="writers-author">
-            <p className="writers-p">Holly Moore</p>
+            <p className="writers-p">Eget Velit</p>
           </div>
           <div className="writers-paragraph">
-            <p className="writers-p">April 19, 2021</p>
+            <p className="writers-p">April 5, 2021</p>
           </div>
         </div>
       </div>
       <div className="writers-container">
         <div className="writers-Header">
-          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+          <h1> Quam Pellentesque Nec Nam</h1>
         </div>
         <div className="WritersPContainer">
           <div className="writers-author">
-            <p className="writers-p">Holly Moore</p>
+            <p className="writers-p">Aliquet Sagittis</p>
           </div>
           <div className="writers-paragraph">
-            <p className="writers-p">April 19, 2021</p>
+            <p className="writers-p">April 3, 2021</p>
           </div>
         </div>
       </div>
       <div className="writers-container">
         <div className="writers-Header">
-          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+          <h1>Cemper Auctor Neque Vitae Tempus </h1>
         </div>
         <div className="WritersPContainer">
           <div className="writers-author">
-            <p className="writers-p">Holly Moore</p>
+            <p className="writers-p">Purus Ut</p>
           </div>
           <div className="writers-paragraph">
-            <p className="writers-p">April 19, 2021</p>
+            <p className="writers-p">April 1, 2021</p>
           </div>
         </div>
       </div>
       <div className="writers-container">
         <div className="writers-Header">
-          <h1>Crypto Breaches Soar: A Deeper Look</h1>
+          <h1>Gravida Neque Convallis a Cras </h1>
         </div>
         <div className="WritersPContainer">
           <div className="writers-author">
-            <p className="writers-p">Holly Moore</p>
+            <p className="writers-p">Id Consectetur</p>
           </div>
           <div className="writers-paragraph">
-            <p className="writers-p">April 19, 2021</p>
+            <p className="writers-p">March 31, 2021</p>
           </div>
         </div>
       </div>
