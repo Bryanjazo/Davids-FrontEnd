8cfb0626e6a6971929013ebc404a5788e57bd985
Bryan Jazo <bryanjazo88@gmail.com>
2021-10-20 19:56:47 -0700

    making all slugs work

--- src/Components/Card/HeadlineCard.jsx
+++ src/Components/Card/HeadlineCard.jsx
@@ -1,10 +1,9 @@
 import "./HeadlineCard.css";
 import test from "../images/test.png";
 import "./HeadlineCard.css";
-import TwoCoins from "../images/TwoCoins.png";
-import DarkBitcoin from "../images/DarkBitcoin.png";
-import ManCoin from "../images/ManCoin.png";
+
 import moment from "moment";
+import { Link } from "react-router-dom";
 export default function HeadlineCard(blogs) {
   return (
     <div className="card-container">
@@ -12,26 +11,32 @@ export default function HeadlineCard(blogs) {
         blog
           .filter((info) => info.categories[0] === "Headlines")
           .map((arr) => (
-            <div className="hl-card-headline">
-              <div className="header">
-                <h1> {arr.title} </h1>
+            <Link
+              className="LinkText"
+              to={"/Blogs/" + arr.slug.current}
+              key={arr.slug.current}
+            >
+              <div className="hl-card-headline">
+                <div className="header">
+                  <h1> {arr.title} </h1>
+                </div>
+                <div className="sub-header">
+                  <p className="category">
+                    <i class="far fa-clone"></i> {arr.categories[0]}
+                  </p>
+                  <p className="date">
+                    <i class="far fa-calendar"></i>{" "}
+                    {moment(arr._createdAt).format("MMMM Do YYYY")}
+                  </p>
+                  <p className="author">
+                    <i class="far fa-user-circle"></i> {arr.authorName}
+                  </p>
+                </div>
+                <div className="img">
+                  <img src={arr.mainImage.asset.url} alt="" id="img" />
+                </div>
               </div>
-              <div className="sub-header">
-                <p className="category">
-                  <i class="far fa-clone"></i> {arr.categories[0]}
-                </p>
-                <p className="date">
-                  <i class="far fa-calendar"></i>{" "}
-                  {moment(arr._createdAt).format("MMMM Do YYYY")}
-                </p>
-                <p className="author">
-                  <i class="far fa-user-circle"></i> {arr.authorName}
-                </p>
-              </div>
-              <div className="img">
-                <img src={arr.mainImage.asset.url} alt="" id="img" />
-              </div>
-            </div>
+            </Link>
           ))
       )}
     </div>
